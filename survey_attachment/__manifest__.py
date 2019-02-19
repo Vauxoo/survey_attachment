7bf4f8e876bcff152f8aab2eb17d4c2ff389a56f
imed.chaabouni <imed.chaabouni@fogits.com>
2019-02-19 12:19:09 +0100

    set image to a big screenshot

--- survey_attachment/__manifest__.py
+++ survey_attachment/__manifest__.py
@@ -12,6 +12,7 @@
     'author': 'Fogits Solutions',
     'website': 'https://www.fogits.com/',
     'depends': ['survey'],
+    'images': ['static/description/survey.jpg'],
     'data': [
         'views/survey_question_template.xml',
         'views/survey_user_input_line_view.xml',
